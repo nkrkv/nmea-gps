0f3f362754b8b82352a69010a7313807684981b6
Victor Nakoryakov <nail.xx@gmail.com>
2018-03-27 16:45:53 +0300

    feat: calculate checksum

--- reject-bad-checksum/patch.cpp
+++ reject-bad-checksum/patch.cpp
@@ -6,6 +6,37 @@ struct State {
 
 void evaluate(Context ctx) {
     XString str = getValue<input_IN>(ctx);
-    // TODO: actually check
-    emitValue<output_OUT>(ctx, str);
+
+    auto it = str.iterate();
+    if (*it != '$') {
+        // Should start with $ and the $ is not accounted in
+        // the checksum
+        return;
+    }
+
+    ++it;
+
+    uint8_t checksum = 0;
+    while (it) {
+        char c = *it;
+        if (c == '*')
+            break;
+
+        checksum ^= c;
+        ++it;
+    }
+
+    if (*it != '*') {
+        // Sudden end of line, the command is malformed
+        return;
+    }
+
+    char checksumStr[3];
+    checksumStr[0] = *(++it);
+    checksumStr[1] = *(++it);
+    checksumStr[2] = '\0';
+    uint8_t expectedChecksum = strtoul(checksumStr, nullptr, 16);
+
+    if (expectedChecksum == checksum)
+        emitValue<output_OUT>(ctx, str);
 }
